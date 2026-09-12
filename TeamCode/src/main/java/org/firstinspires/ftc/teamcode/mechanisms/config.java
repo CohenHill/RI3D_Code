6862f0c79bf0d3ce4f652aa3b6a2845a6cf7e14a
K0lby <kolby35763@gmail.com>
2026-09-12 18:14:37 -0500

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/config.java
@@ -9,33 +9,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class config {
 
-    Drivetrain conf = new Drivetrain();
-    public DcMotor back_left_drive;
-    public DcMotor front_left_drive;
-    public DcMotor back_right_drive;
-    public DcMotor front_right_drive;
-    public DcMotorEx launch_motor_1;
-    public DcMotorEx launch_motor_2;
-    public DcMotor intake_motor;
-    public Servo franklin_flipper_right;
-    public Servo franklin_flipper_left;
-    public Limelight3A limelight;
-    public DistanceSensor pollenDistance;
-    public DistanceSensor nectorDistance;
+    Drivetrain drivetrain = new Drivetrain();
+    Intake intake = new Intake();
+    Flywheels flywheels = new Flywheels();
+    Flippers flippers = new Flippers();
 
     public void init(HardwareMap hwMap) {
-        back_left_drive = hwMap.get(DcMotor.class, "back_left_drive");
-        front_left_drive = hwMap.get(DcMotor.class, "front_left_drive");
-        back_right_drive = hwMap.get(DcMotor.class, "back_right_drive");
-        front_right_drive = hwMap.get(DcMotor.class, "front_right_drive");
-        launch_motor_1 = hwMap.get(DcMotorEx.class, "launch_motor_1");
-        launch_motor_2 = hwMap.get(DcMotorEx.class, "launch_motor_2");
-        intake_motor = hwMap.get(DcMotor.class, "intake_motor");
-        franklin_flipper_right = hwMap.get(Servo.class, "franklin_flipper_right");
-        franklin_flipper_left = hwMap.get(Servo.class, "franklin_flipper_left");
-        limelight = hwMap.get(Limelight3A.class, "limelight");
-        pollenDistance = hwMap.get(DistanceSensor.class, "pollenDistance");
-        nectorDistance = hwMap.get(DistanceSensor.class, "nectorDistance");
+        drivetrain.init(hwMap);
+        intake.init(hwMap);
+        flywheels.init(hwMap);
+        flippers.init(hwMap);
+
+
     }
 }
 
