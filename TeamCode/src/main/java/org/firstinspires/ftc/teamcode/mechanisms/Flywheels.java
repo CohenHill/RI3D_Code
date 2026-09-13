af339d364601b472df51e1ad309d8d92e5699171
CohenHill <cohen.hill@ascte.org>
2026-09-12 21:01:03 -0500

    Update Flywheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Flywheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Flywheels.java
@@ -10,7 +10,8 @@ public class Flywheels {
 
     public DcMotorEx launch_motor_1;
     public DcMotorEx launch_motor_2;
-    private double Kp = 0.0,Ki = 0.00,Kd = 0.0,Kf = 0.0;
+    private double Kp1 = 0.0,Ki1 = 0.0,Kd1 = 0.0,Kf1 = 0.0;
+    private double Kp2 = 0.0,Ki2 = 0.0,Kd2 = 0.0,Kf2 = 0.0;
     public PIDFController flywheelController1, flywheelController2;
 
     private double currentVelocity1, currentVelocity2;
@@ -22,8 +23,8 @@ public class Flywheels {
         launch_motor_1 = hwMap.get(DcMotorEx.class, "launch_motor_1");
         launch_motor_2 = hwMap.get(DcMotorEx.class, "launch_motor_2");
 
-        flywheelController1 = new PIDFController(Kp, Ki, Kd, Kf);
-        flywheelController2 = new PIDFController(Kp, Ki, Kd, Kf);
+        flywheelController1 = new PIDFController(Kp1, Ki1, Kd1, Kf1);
+        flywheelController2 = new PIDFController(Kp2, Ki2, Kd2, Kf2);
 
         launch_motor_1.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         launch_motor_2.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
@@ -49,5 +50,17 @@ public class Flywheels {
         launch_motor_1.setPower(Math.max(0, Math.min(1, output1)));
         launch_motor_2.setPower(Math.max(0, Math.min(1, output2)));
     }
+
+    public void setTargetVelocity(double velocity1, double velocity2) {
+        targetVelocity1 = velocity1;
+        targetVelocity2 = velocity2;
+    }
+    public void setTargetVelocity1(double velocity1) {
+        targetVelocity1 = velocity1;
+    }
+    public void setTargetVelocity2(double velocity2) {
+        targetVelocity2 = velocity2;
+    }
+
 
 }
